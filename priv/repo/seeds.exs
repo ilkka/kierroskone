7091f09ccf61c7c728aa1afe5e157b9e7f5394bd
Ilkka Poutanen <ilkka.poutanen@futurice.com>
2022-03-20 18:52:42 +0200

    Add some seed data

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,34 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+alias Kierroskone.Repo
+alias Kierroskone.{Games.Game, Cars.Car, Cars.Class, Tracks.Track}
+
+dirt =
+  Repo.insert!(%Game{
+    name: "Dirt Rally 2.0"
+  })
+
+ac =
+  Repo.insert!(%Game{
+    name: "Assetto Corsa"
+  })
+
+group_a =
+  Repo.insert!(%Class{
+    name: "Modern Rally Group A",
+    game_id: dirt.id
+  })
+
+evo =
+  Repo.insert!(%Car{
+    name: "Mitsubishi Lancer Evolution VI",
+    class_id: group_a.id,
+    game_id: dirt.id
+  })
+
+oksala =
+  Repo.insert!(%Track{
+    name: "Oksala",
+    game_id: dirt.id
+  })
