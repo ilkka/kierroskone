e3704894565434afbadc8a6fc1832aa68e11ff02
Ilkka Poutanen <ilkka.poutanen@futurice.com>
2022-03-25 20:47:35 +0200

    Port track list and show code from liveview
    Shows basically the same stuff

--- lib/kierroskone_web/controllers/page_controller.ex
+++ lib/kierroskone_web/controllers/page_controller.ex
@@ -1,15 +1,28 @@
 defmodule KierroskoneWeb.PageController do
   use KierroskoneWeb, :controller
+  alias Kierroskone.Tracks
 
   def index(conn, _params) do
     render(conn, "index.html")
   end
 
   def tracks(conn, _params) do
-    render(conn, "tracks.html")
+    conn
+    |> assign(
+      :tracks,
+      Tracks.list_tracks() |> Enum.map(fn t -> {t, Tracks.get_overall_record(t)} end)
+    )
+    |> render("tracks.html")
   end
 
   def track(conn, %{"id" => trackId}) do
-    render(conn, "track.html")
+    track = Tracks.get_track!(trackId)
+
+    conn
+    |> assign(:track, track)
+    |> assign(:laptimes, Tracks.get_laptimes(track))
+    |> assign(:record, Tracks.get_overall_record(track))
+    |> assign(:records, Tracks.get_records_per_car(track))
+    |> render("track.html")
   end
 end
