4c2473266c055f2f26dc814b9ff99a0b32cc1a24
Ilkka Poutanen <ilkka.poutanen@futurice.com>
2022-03-26 11:28:00 +0200

    Flesh out upload script a bit more

--- watch-and-send.ps1
+++ watch-and-send.ps1
@@ -1,4 +1,25 @@
+<#
+.DESCRIPTION
+Watch for new Dirt 2.0 laptimes in a loop and send them to the laptime API.
+#>
+[CmdletBinding()]
+param (
+	# The API endpoint URL for uploading the laptimes. For a Kierroskone instance running at https://kk.example.com the URL should be https://kk.example.com/api/laptime-import/dirt2. 
+	[Parameter(Mandatory = $true)]
+	[Uri]
+	$ApiUrl,
+
+	# Path to Dirt laptime recorder thingy.
+	[Parameter(Mandatory = $true)]
+	[System.IO.FileInfo]
+	$LaptimePath
+)
+
 do {
-	Write-Host "Moro!"
-	Start-Sleep -Seconds 2
+	Write-Host "Here I would do the export command in $LaptimePath but can't remember what it is"
+	Write-Host "Here I would check if the CSV changed"
+	Write-Host "Uploading"
+	Import-Csv -Delimiter ';' ${LaptimePath}\laptimes.csv | ConvertTo-Json -AsArray | Invoke-RestMethod -Method Post -ContentType "application/json" $ApiUrl
+	Write-Host "Sleeping 10 seconds"
+	Start-Sleep -Seconds 10
 } while ($true)
