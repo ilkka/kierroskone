37a687eb470d8fef0add7f6e8dc04be677e3ccb0
Ilkka Poutanen <ilkka.poutanen@futurice.com>
2022-03-26 17:26:24 +0200

    Hopefully final fix to runtime config access

--- lib/kierroskone_web/check_api_auth.ex
+++ lib/kierroskone_web/check_api_auth.ex
@@ -4,13 +4,12 @@ defmodule KierroskoneWeb.CheckApiAuth do
   def init(opts),
     do: opts
 
-  def call(%Plug.Conn{} = conn, opts) do
-    %{"laptime_api_token" => api_token} =
-      Application.fetch_env!(:kierroskone, KierroskoneWeb.Endpoint)
+  def call(%Plug.Conn{} = conn, _opts) do
+    config = Application.fetch_env!(:kierroskone, KierroskoneWeb.Endpoint)
 
     case get_req_header(conn, "x-api-token") do
       [token] ->
-        if token != api_token do
+        if token != config[:laptime_api_token] do
           raise "nope"
         end
 
