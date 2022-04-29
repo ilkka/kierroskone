28c5b48cab24549d64388575e6ff21aa6c2220cd
Ilkka Poutanen <ilkka.poutanen@futurice.com>
2022-04-29 17:22:18 +0300

    Restructure dirt2 upload controller

--- lib/kierroskone_web/controllers/dirt_2_laptimes_upload_controller.ex
+++ lib/kierroskone_web/controllers/dirt_2_laptimes_upload_controller.ex
@@ -3,36 +3,60 @@ defmodule KierroskoneWeb.Dirt2LaptimesUploadController do
   require Logger
   alias Kierroskone.{Cars, Games, Tracks}
 
+  @laptimes_schema %{
+                     "type" => "array",
+                     "items" => %{
+                       "type" => "object",
+                       "properties" => %{
+                         "Car" => %{"type" => "string"},
+                         "Track" => %{"type" => "string"},
+                         "Date" => %{"type" => "string"},
+                         "Time" => %{"type" => "string"},
+                         "Topspeed" => %{"type" => "string"},
+                         "Telemetry" => %{"type" => "array"}
+                       },
+                       "required" => ["Car", "Track", "Date", "Time", "Topspeed"]
+                     }
+                   }
+                   |> ExJsonSchema.Schema.resolve()
+
+  defp get_or_create_game() do
+    case Games.get_game_by_name("Dirt Rally 2.0") do
+      nil ->
+        {:ok, dirt} = Games.create_game(%{"name" => "Dirt Rally 2.0"})
+        dirt
+
+      dirt ->
+        dirt
+    end
+  end
+
+  defp get_or_create_car(car_name, game) do
+    case Cars.get_car_by_name(car_name, game) do
+      nil ->
+        {:ok, car} = Cars.create_car(%{"name" => car_name, "game_id" => game.id})
+        car
+
+      car ->
+        car
+    end
+  end
+
+  defp get_or_create_track(track_name, game) do
+    case Tracks.get_track_by_name(track_name, game) do
+      nil ->
+        {:ok, track} = Tracks.create_track(%{"name" => track_name, "game_id" => game.id})
+        track
+
+      track ->
+        track
+    end
+  end
+
   def create(conn, %{"_json" => laptimes}) do
-    dirt =
-      case Games.get_game_by_name("Dirt Rally 2.0") do
-        nil ->
-          {:ok, dirt} = Games.create_game(%{"name" => "Dirt Rally 2.0"})
-          dirt
+    dirt = get_or_create_game()
 
-        dirt ->
-          dirt
-      end
-
-    laptimes_schema =
-      %{
-        "type" => "array",
-        "items" => %{
-          "type" => "object",
-          "properties" => %{
-            "Car" => %{"type" => "string"},
-            "Track" => %{"type" => "string"},
-            "Date" => %{"type" => "string"},
-            "Time" => %{"type" => "string"},
-            "Topspeed" => %{"type" => "string"},
-            "Telemetry" => %{"type" => "array"}
-          },
-          "required" => ["Car", "Track", "Date", "Time", "Topspeed"]
-        }
-      }
-      |> ExJsonSchema.Schema.resolve()
-
-    case ExJsonSchema.Validator.validate(laptimes_schema, laptimes) do
+    case ExJsonSchema.Validator.validate(@laptimes_schema, laptimes) do
       :ok ->
         for %{
               "Car" => car_name,
@@ -42,25 +66,9 @@ defmodule KierroskoneWeb.Dirt2LaptimesUploadController do
               "Topspeed" => _topspeed,
               "Telemetry" => telemetry
             } <- laptimes do
-          car =
-            case Cars.get_car_by_name(car_name, dirt) do
-              nil ->
-                {:ok, car} = Cars.create_car(%{"name" => car_name, "game_id" => dirt.id})
-                car
+          car = get_or_create_car(car_name, dirt)
 
-              car ->
-                car
-            end
-
-          track =
-            case Tracks.get_track_by_name(track_name, dirt) do
-              nil ->
-                {:ok, track} = Tracks.create_track(%{"name" => track_name, "game_id" => dirt.id})
-                track
-
-              track ->
-                track
-            end
+          track = get_or_create_track(track_name, dirt)
 
           if is_nil(Tracks.get_laptime_by_driven_at(date, track)) do
             # No previous laptime with same original timestamp -> good to store as new
