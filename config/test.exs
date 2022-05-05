96176fd12fae958455a17c9516c693297b7ae245
Ilkka Poutanen <ilkka.poutanen@futurice.com>
2022-05-05 14:55:55 +0300

    Fix db username for testing

--- config/test.exs
+++ config/test.exs
@@ -6,7 +6,7 @@ import Config
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
 config :kierroskone, Kierroskone.Repo,
-  username: "postgres",
+  username: "kierroskone",
   password: "postgres",
   hostname: "localhost",
   database: "kierroskone_test#{System.get_env("MIX_TEST_PARTITION")}",
